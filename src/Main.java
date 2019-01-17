8565dd4c709dff7fe686a27c061b3e2dbcda6771
qq570850096 <someone741@qq.com>
2019-01-17 13:05:15 +0800

    加入了迷宫的文件

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
 
 public class Main{
-    
+
+    public static void main(String[] args) {
+        String mazefile="maze_101_101.txt";
+
+        MazeData data=new MazeData(mazefile);
+        data.print();
+    }
+
 }
